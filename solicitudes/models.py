148f9852e13c32141f8bed26cc1da703e42dd825
miikeepp <michael.lagos@campusucc.edu.co>
2026-04-16 09:06:18 -0500

    avance solicitud models

--- solicitudes/models.py
+++ solicitudes/models.py
@@ -12,22 +12,11 @@ class Solicitud(models.Model):
     documento_identidad = models.CharField(max_length=50)
     correo = models.EmailField()
     telefono = models.CharField(max_length=20)
-
-    tipo_solicitud = models.CharField(
-        max_length=20,
-        choices=TIPOS_SOLICITUD
-    )
-
+    tipo_solicitud = models.CharField(max_length=20, choices=TIPOS_SOLICITUD)
     asunto = models.CharField(max_length=150)
     descripcion = models.TextField()
-
     fecha_solicitud = models.DateField()
-
-    archivo_adjunto = models.FileField(
-        upload_to='solicitudes_archivos/',
-        blank=True,
-        null=True
-    )
+    archivo_adjunto = models.FileField(upload_to='solicitudes_archivos/', blank=True, null=True)
 
     def __str__(self):
-        return f"{self.nombre_solicitante} - {self.tipo_solicitud}"
+        return self.nombre_solicitante
