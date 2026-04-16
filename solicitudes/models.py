2f70a1498f766a42da65eb5e5f71489cb079ff36
miikeepp <michael.lagos@campusucc.edu.co>
2026-04-16 08:56:38 -0500

    Add models

--- solicitudes/models.py
+++ solicitudes/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Solicitud(models.Model):
     TIPOS_SOLICITUD = [
         ('academica', 'Académica'),
@@ -14,12 +11,23 @@ class Solicitud(models.Model):
     nombre_solicitante = models.CharField(max_length=150)
     documento_identidad = models.CharField(max_length=50)
     correo = models.EmailField()
-    telefono = models.IntegerField()
-    tipo_solicitud = models.CharField(max_length=20, choices=TIPOS_SOLICITUD)
+    telefono = models.CharField(max_length=20)
+
+    tipo_solicitud = models.CharField(
+        max_length=20,
+        choices=TIPOS_SOLICITUD
+    )
+
     asunto = models.CharField(max_length=150)
     descripcion = models.TextField()
+
     fecha_solicitud = models.DateField()
-    archivo_adjunto = models.FileField(upload_to='archivos/', blank=True, null=True)
+
+    archivo_adjunto = models.FileField(
+        upload_to='solicitudes_archivos/',
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
-        return self.nombre_solicitante
+        return f"{self.nombre_solicitante} - {self.tipo_solicitud}"
